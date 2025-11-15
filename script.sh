44632e8ea4a9562f84be78f1cd5e17d39d86f5fb
Akash Arjun Dalvi <157683004+akash-dalvi22@users.noreply.github.com>
2025-11-15 18:06:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ echo "This is the first line of the script"
 echo "Welcome to the NetPath Devops"
 echo "We are going to learn git merge"
 echo "This is the last line of the code"
-
+echo "This is the new line of feature"
